a2ba08746db7196145c97c59cffbe039346b8acb
TekclasMario <52262813+TekclasMario@users.noreply.github.com>
2019-06-26 16:31:46 -0700

    Update and rename HelloWorld.vue to Main.vue

--- Main.vue
+++ Main.vue
@@ -1,26 +1,14 @@
 <template>
   <div >
-    <!-- top section  -->
-
-    <!-- Mid_Section -->
-
     <div id="red-banner">
       <h1 class="text-xs-center pt-2 white--text">{{ $t("About") }}</h1>
     </div>
-
-
     <div class="mt-5" id="red-banner">
       <h1 class="text-xs-center pt-2 white--text">{{ $t("Flavors") }}</h1>
     </div>
-
-   
     <div class="mt-1 pa-2" id="red-banner">
       <h1 class="text-xs-center white--text">{{ $t("Info") }}</h1>
     </div>
-    
-    
-    
-   
   </div>
 </template>
 
